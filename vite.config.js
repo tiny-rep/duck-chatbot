cddcc76ed2bca940c57ba5ab193d14efd18b19c4
samDragon <kxhaibin@qq.com>
2026-03-26 22:08:25 +0800

    1 增加登录页，并实现通过用户昵称+密码进行登录
    Signed-off-by: samDragon <kxhaibin@qq.com>

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import {
+  defineConfig
+} from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
+
+export default defineConfig({
+  plugins: [uni()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    },
+  }
+});
