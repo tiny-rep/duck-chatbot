fc0a6316f56ae4a4d1511b6588406dcd1d7b1eb0
samDragon <kxhaibin@qq.com>
2026-04-07 22:16:07 +0800

    1 实现登录名、模型列表的基本加载逻辑
    Signed-off-by: samDragon <kxhaibin@qq.com>

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:5000', //'http://ai-demo.b-nature.cn',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
