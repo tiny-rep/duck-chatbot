094ae0a44ed5a58c3ccdc600bc2fd03e6ca63700
samDragon <kxhaibin@qq.com>
2026-05-07 17:49:51 +0800

    1 完善条件编译
    Signed-off-by: samDragon <kxhaibin@qq.com>

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import uni from '@dcloudio/vite-plugin-uni';
 export default defineConfig({
   plugins: [uni()],
   server: {
+	port: 3000,
     proxy: {
       '/api': {
         target: 'http://ai-demo.b-nature.cn', //http://127.0.0.1:5000 http://ai-demo.b-nature.cn,
